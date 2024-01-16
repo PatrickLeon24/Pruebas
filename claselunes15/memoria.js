61ed8c41add4427b0b6cc3b400966e337193862b
PatrickLeon24 <20211454@ALOE.ULIMA.EDU.PE>
2024-01-16 12:19:49 -0500

    ol

--- claselunes15/memoria.js
+++ claselunes15/memoria.js
@@ -1,7 +1,11 @@
 let simbolos = ["&#129300;", "&#128526;","&#128528;"]
-document.write("ay")
+
 let listaCasillas=[]
 
+
+let casillaTurnoAnterior=null
+
+let numeroTurno=1
 function CrearCasillas(){
 
     
@@ -32,11 +36,86 @@ function ponerSimbolosCasillas(){
         for (let j = 0; j < 3; j++) {
             const but =document.getElementById(i+"_"+j)
             const casilla =devolverCasilla(i,j)
-            but.innerHTML= casilla.simbolo
+            if (casilla.mostrandoSimbolo) {
+                but.innerHTML= casilla.simbolo
+            }
+            else{
+                but.innerHTML= "UL"
+            }
+            
             
         }
         
     }
+}
+
+function mostrarEnCasoDeError() {
+    
+}
+let casillaId=[]
+function casillaOnClick(i,j){
+    console.log("Hizo Click")
+    
+    const casilla =devolverCasilla(i,j)
+    
+    if (numeroTurno==1) {
+        numeroTurno=2
+        casilla.mostrandoSimbolo=true
+        casillaTurnoAnterior=casilla
+        
+        casillaId.push(i)
+        casillaId.push(j)
+        
+
+
+        ponerSimbolosCasillas()
+    }
+    else{
+        
+        casilla.mostrandoSimbolo=true
+        
+        if (casillaTurnoAnterior.simbolo != casilla.simbolo) {
+            //ocultarse
+            
+            
+            ponerSimbolosCasillas()
+
+            setTimeout(function(){
+
+                casilla.mostrandoSimbolo=false
+                casillaTurnoAnterior.mostrandoSimbolo=false
+                ponerSimbolosCasillas()
+                casillaTurnoAnterior=null
+                numeroTurno=1
+            }, 2000)
+            console.log("sigma Click")
+            casillaId.pop()
+            casillaId.pop()
+        }
+        else{
+
+            casillaTurnoAnterior=null
+            
+            ponerSimbolosCasillas()
+            
+            let antiCheat1 =document.getElementById(i+"_"+j)
+            let antiCheat2 =document.getElementById(casillaId[0]+"_"+casillaId[1])
+            console.log(i+"_"+j)
+            console.log(casillaId[0]+"_"+casillaId[1])
+
+            antiCheat1.disabled= true
+            antiCheat2.disabled= true
+            casillaId.pop()
+            casillaId.pop()
+            console.log(casillaId[0]+"_"+casillaId[1])
+            numeroTurno=1
+            
+        }
+        
+
+    }
+    
+
 }
 function main(){
     CrearCasillas()
